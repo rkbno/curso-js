aed3793c0ba28846c8228f358558b485ae552478
rk <brenotm10@gmail.com>
2022-12-07 19:45:35 -0300

    estudos

--- ex.da.aula012/ex01/script.js
+++ ex.da.aula012/ex01/script.js
@@ -4,21 +4,21 @@ function carregar () {
     var data = new Date()
    var hora = data.getHours()
    
-    msg.innerHTML = 'Agora são ' + hora + ' horas.'
+    
     if (hora >= 0 && hora < 12) {
         // bom dia
         img.src = 'imagens/manhã.jpg'
         document.body.style.background = '#e2cd9f'
-        msg.innerHTML = 'Bom dia'
+        msg.innerHTML = '<h1>Bom dia</h1>'
     } else if (hora >= 12 && hora < 18) {
         // boa tarde
         img.src = 'imagens/tarde.jpg'
         document.body.style.background = '#b9846e'
-        msg.innerHTML = 'Boa tarde'
+        msg.innerHTML = '<h1>Boa tarde</h1>'
     } else {
         // boa noite
         img.src = 'imagens/noite.jpg'
         document.body.style.background = '#515151'
-        msg.innerHTML = 'Boa noite'
+        msg.innerHTML = '<h1>Boa noite</h1>'
     }
 }
