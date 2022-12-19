87d14733978e548ced1bb37a2de795bf08034d8d
rk <brenotm10@gmail.com>
2022-12-19 10:56:29 -0300

    estudos

--- ex.da.aula014/ex016/script.js
+++ ex.da.aula014/ex016/script.js
@@ -1,8 +1,31 @@
-function verificar() {
-    var txti = document.querySelector('input#txtinicio')
-    var txtf = document.querySelector('input#txtfim')
-    var txtp = document.querySelector('input#txtpasso')
+function contar() {
+    var ini = document.querySelector('input#txti')
+    var fim = document.querySelector('input#txtf')
+    var passo = document.querySelector('input#txtp')
+    var res = document.querySelector('div#res')
     
-    var res = txti + txtf
-    res.innerHTML = 'O Valor é' + res
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ) { 
+        res.innerHTML = 'Impossivel contar!'
+        // alert('[ERROR] Faltam dados!')
+        } else {
+            res.innerHTML = 'Contando... <br>'
+            var i = Number(ini.value)
+            var f = Number(fim.value)
+            var p = Number(passo.value)
+            if (p <= 0) {
+                window.alert('Passo inválido! Considerando PASSO 1')
+                p = 1
+            }
+            if (i < f) {
+
+                for(var c = i ; c <= f ; c += p ) {
+                    res.innerHTML +=  c + ' \u{1F449}'
+                } 
+            } else { 
+                for (var c = i; c >= f; c -= p) {
+                    res.innerHTML += c + ' \u{1F449}'
+                }
+            }
+            res.innerHTML += '\u{1F3C1}'
+        }
 }
