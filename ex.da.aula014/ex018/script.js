48336b2f4092a80a8e7a869956155a575695f3df
rk <brenotm10@gmail.com>
2022-12-26 10:58:46 -0300

    estudos

---
+++ ex.da.aula014/ex018/script.js
@@ -0,0 +1,13 @@
+function adicionar() {
+    var num = document.querySelector('input#txtn')
+    var marc = document.querySelector('input#marcar')
+
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número!')
+    }
+
+}
+
+function verificar() {
+    
+}
