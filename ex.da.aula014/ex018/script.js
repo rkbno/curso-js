42673d9843da591a9f4734b59bf4a732847fde5f
rk <brenotm10@gmail.com>
2022-12-31 12:11:37 -0300

    ultimo exercicio do estudo

--- ex.da.aula014/ex018/script.js
+++ ex.da.aula014/ex018/script.js
@@ -1,3 +1,8 @@
+var num = document.querySelector('input#fnum')
+var lista = document.querySelector('select#flista')
+var res = document.querySelector('div#res')
+var valores = []
+
 function isNumero(n) {
  if (Number(n) >= 1 && Number(n) <= 100) {
     return true
@@ -6,8 +11,8 @@ function isNumero(n) {
  }
 }
 
-function inLista(n, 1) {
-    if (length.indexOf(Number(n)) != -1) {
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
         return true 
         } else {
             return false
@@ -17,16 +22,43 @@ function inLista(n, 1) {
 
 
 function adicionar() {
-    var num = document.querySelector('input#fnum')
-    var lista = document.querySelector('select#flista')
-    var res = document.querySelector('div#res')
-    var valores = []
 
     if (isNumero(num.value) && !inLista(num.value, valores)) {
-        alert('tudo ok!')
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = 'Valor ' + num.value + ' adicionado.'
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         alert('Valor inválido ou já encontrado na lista')
     }
-
+    num.value = ''
+    num.focus()
 }
 
+
+function finalizar () {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar!')
+    } else {
+        var tot = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        var soma = 0
+        var media = 0
+        for(var pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+            maior = valores[pos]
+            if (valores[pos] < menor)
+            menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += '<p>Ao todo, temos ' + tot + ' números cadastrados. </p>'
+        res.innerHTML += '<p>O maior valor informado foi ' + maior + ' </p>'
+        res.innerHTML += '<p>O menor valor informado foi ' + menor + ' </p>'
+        res.innerHTML += '<p>A soma de todos os valores, é ' + soma + ' </p>'
+        res.innerHTML += '<p>A média dos valores digitados é ' + media + ' </p>'
+    }
+}
