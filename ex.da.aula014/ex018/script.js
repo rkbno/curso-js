79ca68c867cf5dc1b6f6065f274c0ad9a5fc96f5
rk <brenotm10@gmail.com>
2022-12-31 10:24:29 -0300

    ajutes

--- ex.da.aula014/ex018/script.js
+++ ex.da.aula014/ex018/script.js
@@ -1,13 +1,32 @@
-function adicionar() {
-    var num = document.querySelector('input#txtn')
-    var marc = document.querySelector('input#marcar')
+function isNumero(n) {
+ if (Number(n) >= 1 && Number(n) <= 100) {
+    return true
+ } else {
+    return false
+ }
+}
 
-    if (num.value.length == 0) {
-        alert('Por favor, digite um número!')
+function inLista(n, 1) {
+    if (length.indexOf(Number(n)) != -1) {
+        return true 
+        } else {
+            return false
+        }
+    }
+
+
+
+function adicionar() {
+    var num = document.querySelector('input#fnum')
+    var lista = document.querySelector('select#flista')
+    var res = document.querySelector('div#res')
+    var valores = []
+
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        alert('tudo ok!')
+    } else {
+        alert('Valor inválido ou já encontrado na lista')
     }
 
 }
 
-function verificar() {
-    
-}
