3f7dab063fac2a53cfb702114733d3779280db7b
rk <brenotm10@gmail.com>
2023-01-01 23:25:22 -0300

    Update objeto01.js

--- aula017/objeto01.js
+++ aula017/objeto01.js
@@ -1,11 +1,10 @@
-    let amigo = {nome: 'josé', 
-    sexo: 'M', 
-    peso: '85.4',
-    engordar(p=0){
-        console.log('Engordou')
-        this.peso += p
-    }}
+    let amigo = {nome: 'José',
+sexo: 'M',
+peso: 85.4,
+engordar(p=0){
+    console.log('Engordou')
+    this.peso += p
+}}
 
-
-    amigo.engordar(2)
-console.log(amigo.nome + ' pesa ' + amigo.peso + ' Kg.')
+amigo.engordar(2)
+console.log(amigo.nome + ' pesa ' + amigo.peso + ' Kg')
